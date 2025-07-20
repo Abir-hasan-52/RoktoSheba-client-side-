429231bef7dc01e6905763f773a7d3b73bbae31a
Abir-Hasan-52 <abirhasan5208+github@gmail.com>
2025-07-20 16:28:06 +0600

    AuthProver a createUser singinUse, logOut,and set a observer

--- src/Contexts/AuthProvider.jsx
+++ src/Contexts/AuthProvider.jsx
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../Firebase/firebase.init";
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // createUser
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  // signIn user
+
+  const signInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  //   Log out
+
+  const logOutUser = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log("user in the auth state change", currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
   const authInfo = {
-
-
-    
+    user,
+    loading,
+    createUser,
+    signInUser,
+    logOutUser,
   };
   return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
